77c15786182191062f6a539913a3f5e45ceaf3d6
Martin Karp <martinkarp@n156-p154.eduroam.kth.se>
2020-01-28 15:02:01 +0100

    Removed unnecessary lib

--- IntelSamples/MultiDeviceBasic/cpu+mic.system-level.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-./multidevice -t cpu -c system --instance-count 2 --instance-index 0 $* > cpu+mic.system-level.cpu.out &
-./multidevice -t acc -c system --instance-count 2 --instance-index 1 $* > cpu+mic.system-level.acc.out &
-
-wait
-
